46963c2f8159953a539d01a52fb2cd6333d35277
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-26 16:01:17 +0600

    Arrow Function

---
+++ arrow-function.js
@@ -0,0 +1,11 @@
+const add =(num1,num2) => num1+num2
+
+console.log(add(10,12))
+
+const get= (num1,num2) => {
+    const add = num1+num2;
+    const diff = num1-num2;
+
+    const total = add + diff;
+    return total;
+}
